77a713c962d80822c2029f37a2ac0ed25ee6d4b4
pokio <32353873+pokiiio@users.noreply.github.com>
2017-12-05 23:06:00 +0900

    Add files via upload

---
+++ ESP32Keikyu.h
@@ -0,0 +1,29 @@
+#ifndef ESP32KEIKYU_H
+#define ESP32KEIKYU_H
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class ESP32Keikyu
+{
+public:
+  int getStatus(); // 下で定義している定数を返します
+
+private:
+  String getUnkoInfo();               // 京急のページから運行情報の文字列を取得します
+  int getUnkoStatus(String unkoInfo); // 運行情報の文字列からステータスを判断します
+};
+
+extern ESP32Keikyu Keikyu;
+
+#define KEIKYU_STATUS_UNKNOWN 0           // 京急の運行情報が取得できません
+#define KEIKYU_STATUS_NORMAL 1            // 京急は平常通り運転してます
+#define KEIKYU_STATUS_DELAYED 2           // 遅れています
+#define KEIKYU_STATUS_TERRIBLE 3          // ダイヤが乱れています
+#define KEIKYU_STATUS_STOPPED 4           // 運転を見合わせています
+#define KEIKYU_STATUS_DELAYED_DUE_TO_JR 5 // JRからの振替輸送受託のため遅れています
+
+#endif
